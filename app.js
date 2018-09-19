42a6f6418a77671c05f3b89fa43216582f0b95fb
Jock Dalby <jockdalby86@gmail.com>
2018-09-20 05:22:07 +1200

    pull providedLocation, location longitude and location latitude from response body

--- app.js
+++ app.js
@@ -6,5 +6,15 @@ request({
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=${configVar.MAP_QUEST_API_KEY}&location=1301%20lombard%20street%20philadelphia`,
   json: true
 }, (error, response, body) => {
-  console.log(body.results);
+  const results = body.results;
+  const providedLocation = results[0].providedLocation;
+  const latLngInfo = results[0].locations[0].latLng;
+  const locationLongitude = latLngInfo.lng;
+  const locationLatitude = latLngInfo.lat;
+  // Second argument not needed
+  // 3rd argument states 2 spaces per indentation
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log('Provided location: ', providedLocation);
+  console.log('Location longitude: ', locationLongitude);
+  console.log(':ocation latitude: ', locationLatitude);
 });
